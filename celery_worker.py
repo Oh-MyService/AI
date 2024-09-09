f48404e1019047497cb4332086eeca129dde1f2b
charlie3965 <charlie39653965@gmail.com>
2024-09-09 20:13:35 +0900

    message queue와 재연결시에도 재생성하지 않도록 수정

--- celery_worker.py
+++ celery_worker.py
@@ -29,7 +29,7 @@ app.conf.broker_url = "amqp://user:password@rabbitmq:5672//"
 app.conf.broker_connection_retry_on_startup = True
 app.conf.broker_heartbeat = 600
 app.conf.broker_connection_timeout = 120  # 연결 시간 초과를 늘림
-app.conf.task_acks_late = True
+app.conf.task_acks_late = False
 app.conf.task_reject_on_worker_lost = True
 app.conf.worker_cancel_long_running_tasks_on_connection_loss=True
 

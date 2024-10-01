365cb4ee0821b742e47b6868b8872d02097adf83
charlie3965 <charlie39653965@gmail.com>
2024-10-01 14:18:05 +0900

    모델 StS_PonyXL_Detail_Slider_v1.4_iteration_3.safetensors로 변경

--- celery_worker.py
+++ celery_worker.py
@@ -120,7 +120,7 @@ def generate_and_send_image(self, prompt_id, image_data, user_id, options):
         global pipeline
         if pipeline is None:
             pipeline = seamless_tiling(
-                pipeline=prepare_pipeline("/mnt/temp/prefectPonyXL_v3.safetensors"), 
+                pipeline=prepare_pipeline("/mnt/temp/StS_PonyXL_Detail_Slider_v1.4_iteration_3.safetensors"), 
                 x_axis=True, 
                 y_axis=True
             )

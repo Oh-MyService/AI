8a6395c1d78912450507d467b5b30a006e3766a1
charlie3965 <charlie39653965@gmail.com>
2024-10-30 16:45:52 +0900

    model_name 추가

--- celery_worker.py
+++ celery_worker.py
@@ -123,7 +123,7 @@ def generate_and_send_image(self, prompt_id, image_data, user_id, options):
         global pipeline
         if pipeline is None:
             pipeline = seamless_tiling(
-                pipeline=prepare_pipeline("/mnt/temp/juggernautXL_juggXILightningByRD.safetensors"), 
+                pipeline=prepare_pipeline(f"/mnt/temp/{image_data["model_name"]}.safetensors"), 
                 x_axis=True, 
                 y_axis=True
             )
